aa6645515bcfa0c5aff28b7260b2c08fe25fd060
Archit-D2K <i-archit.rivastava@d2kindia.com>
2024-10-01 02:22:32 +0530

    fix routing issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Navbar from './Components/Navbar'; 
+import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/jobs" element={<Jobs />} />
-      </Routes>
-      <Footer /> 
-    </Router>
+         <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/jobs" element={<Jobs />} /> */}
+        </Routes>
+        <Footer />
+        </div>
+    
   );
-}
+};
 
 export default App;
